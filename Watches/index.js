90d663c2aa97d43670f6f0923f633a2ce84dad50
kailash yadav <kailashyadav1998@gmail.com>
2021-04-18 21:18:46 +0530

    Upload

---
+++ Watches/index.js
@@ -0,0 +1,22 @@
+
+Vue.component('header-page',{
+    template:'<h1><strong>BandBucket</strong></h1>'
+})
+// Vue.component('main-page',{
+//     props:['product'],
+//     template:'<li> {{product.title}} <br><span> Price :{{product.price}}</span> </li>'
+// })
+
+let head = new Vue({
+    el:'#appHead',
+    data:{
+        productList:[
+            {id:1,url:"Images/index.jpeg", title:' Gramin 943 for runner', MRP:500,price:200},
+            {id:2,url:"Images/watch1.jpeg",title:' Gramin 943 for runner',MRP:500,price:300},
+            {id:3,url:"Images/watch2.jpeg",title:' Gramin 943 for runner',MRP:500,price:400},
+            {id:4,url:"Images/watch3.jpeg",title:' Gramin 943 for runner',MRP:500,price:500},
+            {id:5,url:"Images/watch4.jpeg",title:' Gramin 943 for runner',MRP:500,price:500},
+        ]
+    }
+})
+// products
